cf76adedef216306c1ffc6b706d2699c6b69eb09
naknamu <jodeldelvalle@gmail.com>
2023-03-01 19:18:26 +0800

    Added new project, images per device screen and edited styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,12 @@ window.addEventListener("scroll", function() {
     }
 })
 
+const blog = document.querySelector("[data-nav-blog]");
+
+blog.addEventListener('click', function() {
+    alert("Feature coming soon!");
+})
+
 
 /**
  * Contact
@@ -54,3 +60,30 @@ window.onbeforeunload = () => {
       form.reset();
     }
 }
+
+
+
+/**
+ * Scroll Reveal
+ * 
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]")
+
+
+const reveal = function () {
+    for (let i=0, len = revealElements.length; i < len; i++) {
+        if (revealElements[i].getBoundingClientRect().top < window.innerHeight) {
+            revealElements[i].classList.add("revealed");
+        }
+    }
+}
+
+for (let i=0, len = revealDelayElements.length; i<len; i++) {
+    revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
