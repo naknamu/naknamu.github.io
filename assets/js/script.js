a0f124f6a2a83609c7651dbd9e11ffd359347090
naknamu <jodeldelvalle@gmail.com>
2023-03-02 19:43:23 +0800

    Added project page and removed android dev skill

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-'use-strict';
-
+"use-strict";
 
 /**
  * add event listener on multiple elements
  */
 
 const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0, len = elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType, callback);
-    }
-}
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
 
 /**
  * NAVBAR TOGGLE FOR MOBILE
@@ -19,14 +18,13 @@ const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
 
-const toggleNavbar = function() {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-    document.body.classList.toggle("nav-active")
-}
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
-
 
 /**
  * HEADER
@@ -35,20 +33,19 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
 
 const header = document.querySelector("[data-header]");
 
-window.addEventListener("scroll", function() {
-    if (window.scrollY > 100) {
-        header.classList.add("active")
-    } else {
-        header.classList.remove("active")
-    }
-})
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
 
 const blog = document.querySelector("[data-nav-blog]");
 
-blog.addEventListener('click', function() {
-    alert("Feature coming soon!");
-})
-
+blog.addEventListener("click", function () {
+  alert("Feature coming soon!");
+});
 
 /**
  * Contact
@@ -56,34 +53,31 @@ blog.addEventListener('click', function() {
  */
 
 window.onbeforeunload = () => {
-    for(const form of document.getElementsByTagName('form')) {
-      form.reset();
-    }
-}
-
-
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
 
 /**
  * Scroll Reveal
- * 
+ *
  */
 
 const revealElements = document.querySelectorAll("[data-reveal]");
-const revealDelayElements = document.querySelectorAll("[data-reveal-delay]")
-
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
 
 const reveal = function () {
-    for (let i=0, len = revealElements.length; i < len; i++) {
-        if (revealElements[i].getBoundingClientRect().top < window.innerHeight) {
-            revealElements[i].classList.add("revealed");
-        }
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight) {
+      revealElements[i].classList.add("revealed");
     }
-}
+  }
+};
 
-for (let i=0, len = revealDelayElements.length; i<len; i++) {
-    revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay =
+    revealDelayElements[i].dataset.revealDelay;
 }
-
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
